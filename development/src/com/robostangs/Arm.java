c91ac268eba082bdbab152fc5f5f192fe2b18800
robostangs <robostangs@d15afd79-671f-455b-9fa3-daa50afe212d>
2013-01-26 19:02:45 +0000

    Constructor and various classes added, including getAngle and other crap. Enjoy. Also the game.
    git-svn-id: https://www.quantumous.com/svn/UA2013/trunk@6 d15afd79-671f-455b-9fa3-daa50afe212d

--- development/src/com/robostangs/Arm.java
+++ development/src/com/robostangs/Arm.java
@@ -6,6 +6,7 @@ package com.robostangs;
 
 import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
 /**
  *
@@ -20,6 +21,66 @@ public class Arm {
     private MotorOutput motors;
     private PIDController pid, backUpPid;
     private boolean useBackUp; 
-    
-    
+    public Arm ()
+    {
+        try {
+            mainPot = new Potentiometer(Constants.ARM_MAIN_POT);
+            backUpPot = new Potentiometer(Constants.ARM_BACKUP_POT);
+            jag1 = new CANJaguar(Constants.ARM_JAG1_POS);
+            jag2 = new CANJaguar(Constants.ARM_JAG2_POS);
+            motors = new MotorOutput(jag1, jag2);
+            pid  = new PIDController(Constants.ARM_PID_KP, Constants.ARM_PID_KI, Constants.ARM_PID_KD, mainPot, motors);
+            backUpPid = new PIDController(Constants.ARM_PID_KP, Constants.ARM_PID_KI, Constants.ARM_PID_KD, backUpPot, motors);
+            pid.setInputRange(Constants.ARM_PID_MIN, Constants.ARM_PID_MAX);
+            backUpPid.setInputRange(Constants.ARM_PID_MIN, Constants.ARM_PID_MAX);
+            pid.setOutputRange(Constants.ARM_POW_MIN, Constants.ARM_POW_MAX);
+            backUpPid.setOutputRange(Constants.ARM_POW_MIN, Constants.ARM_POW_MAX);
+            pid.disable();
+            backUpPid.disable();
+        } catch (CANTimeoutException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    //enable backup POT and PID
+    public void useBackUp()
+    {
+        useBackUp = true;
+     }
+    //disable backup POT and PID
+    public void disableBackUp()
+    {
+        useBackUp = false;
+    }
+    //manually set the speed of the arm motors and disable PID
+    public void speedManual(double speed)
+    {
+        if (pid.isEnable()){
+            pid.disable();
+        } else if (backUpPid.isEnable()){
+            backUpPid.disable();
+        }
+        
+        if (getAngle() < Constants.ARM_ANGLE_MIN) {
+            motors.set(speed * Constants.ARM_POWER_COEFF);
+        } else {
+            motors.set(0);
+        }
+        if (getAngle() > Constants.ARM_ANGLE_MAX) {
+            motors.set(speed * Constants.ARM_POWER_COEFF);
+        } else {
+            motors.set(0);
+        }
+    }
+    //get the angle of the arm
+    public double getAngle()
+    {
+        double angle;
+        if (useBackUp = false) {
+            angle = (mainPot.getPotentiometer() - Constants.);
+        } else {
+            angle = (backUpPot.getPotentiometer() - Constants.);   
+        }
+        return angle;
+    }
 }
