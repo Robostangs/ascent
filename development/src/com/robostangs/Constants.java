b65b08ba722755ad6c7f3f6c78ff476adc70c470
kosh <kosh@d15afd79-671f-455b-9fa3-daa50afe212d>
2013-02-02 17:58:06 +0000

    added constants for drivetrain
    git-svn-id: https://www.quantumous.com/svn/UA2013/trunk@21 d15afd79-671f-455b-9fa3-daa50afe212d

--- development/src/com/robostangs/Constants.java
+++ development/src/com/robostangs/Constants.java
@@ -21,4 +21,23 @@ public class Constants {
      */
     public static final double MICRO_TO_BASE = 0.0;
     
+    /*
+     * DriveTrain
+     */
+    //Pos
+    public static final int DT_LEFT_ENCODER_FRONT = 0;
+    public static final int DT_LEFT_ENCODER_BACK = 0;
+    public static final int DT_RIGHT_ENCODER_FRONT = 0;
+    public static final int DT_RIGHT_ENCODER_BACK = 0;
+    public static final int DT_GYRO_POS = 0;
+    public static final int DT_JAG_POS = 0;
+    public static final int DT_JAG_CLIMB_POS = 0;
+    public static final double DT_JAG_CONFIG_TIME = 0.5;
+    
+    //drive straight
+    public static final double DT_STRAIGHT_LEFT_INC = 0.0;
+    public static final double DT_STRAIGHT_LEFT_DEC = 0.0;
+    public static final double DT_STRAIGHT_RIGHT_INC = 0.0;
+    public static final double DT_STRAIGHT_RIGHT_DEC = 0.0;
+    
 }
