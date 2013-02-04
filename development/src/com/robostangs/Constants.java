e4730d68bc84f855ad734bf8148da30d3df13f27
robostangs <robostangs@d15afd79-671f-455b-9fa3-daa50afe212d>
2013-02-04 02:19:22 +0000

    git-svn-id: https://www.quantumous.com/svn/UA2013/trunk@35 d15afd79-671f-455b-9fa3-daa50afe212d

--- development/src/com/robostangs/Constants.java
+++ development/src/com/robostangs/Constants.java
@@ -15,9 +15,25 @@ public class Constants {
      * StopWatch
      */
     public static final double MICRO_TO_BASE = 0.0;
+// <<<<<<< .mine
+    public static final int POT_A_PORT = 0;
+    public static final int POT_B_PORT = 0;
+    public static final int POT_MIN_VALUE = 0;
+    public static final int POT_MAX_VALUE = 0;
+    public static final double INIT_POT_VALUE = 0.0;
+    public static final double POT_IN_DEGREES = 0.0;
+    // arm pid
+    public static final double ARM_KP_A = 0.0;
+    public static final double ARM_KI_A = 0.0;
+    public static final double ARM_KD_A = 0.0;
+    public static final double ARM_KP_B = 0.0;
+    public static final double ARM_KI_B = 0.0;
+    public static final double ARM_KD_B = 0.0;
+// =======
     static final int INGEST_RELAY = 0;
     static final int LIFTER_JAG = 0;
     static final int INGEST_CONV_JAG = 0;
     static final int SHOOT_CONV_JAG = 0;
     
+// >>>>>>> .r33
 }
