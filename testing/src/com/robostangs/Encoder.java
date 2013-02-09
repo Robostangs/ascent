d02c6c52cf74d665279a438ba41b529f4bf224a6
robostangs <robostangs@d15afd79-671f-455b-9fa3-daa50afe212d>
2013-02-09 19:30:07 +0000

    Encoder Class new TODO
    git-svn-id: https://www.quantumous.com/svn/UA2013/trunk@95 d15afd79-671f-455b-9fa3-daa50afe212d

--- testing/src/com/robostangs/Encoder.java
+++ testing/src/com/robostangs/Encoder.java
@@ -14,7 +14,7 @@ public class Encoder {
     //Initializing Variable Distance
     double distance = 6.7; //Random number for testing purposes
     double distancePerPulse = 0;
-    int pulses = 4; //Random number for testing purposes
+    static int pulses = 4; //Random number for testing purposes
         
     public Encoder (int left, int right){
         
@@ -22,6 +22,7 @@ public class Encoder {
     
     public double getDistance(){
         //TODO: Change the pulses variable to update realisticly
+        //TODO: Log.write should put the left/right encoder was read and include date and time and distance where it was returned
         distance = distancePerPulse * pulses;
         return distance;
     }
