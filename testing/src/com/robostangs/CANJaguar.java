e3e4f9aec5f3a07ec774a2d780c40951b616979a
robostangs <robostangs@d15afd79-671f-455b-9fa3-daa50afe212d>
2013-02-09 18:30:10 +0000

    the jag class hath a few methods now. Praise be the lawd. PS we shoud go for Five Guys runs for the fries they are delicious.
    git-svn-id: https://www.quantumous.com/svn/UA2013/trunk@90 d15afd79-671f-455b-9fa3-daa50afe212d

--- testing/src/com/robostangs/CANJaguar.java
+++ testing/src/com/robostangs/CANJaguar.java
@@ -9,5 +9,22 @@ package com.robostangs;
  * @author VikasV
  */
 public class CANJaguar {
+    private int pos;
+    public CANJaguar()
+    {
+        pos = 0;
+    }
+    public void configFaultTime(Double faultTime)
+    {
+    String logTime = faultTime.toString();
+    Log.write("Fault time configured to: ");
+    Log.write(logTime);
+    }
+    public void setX(Double output)
+    {
+    String logTime = output.toString();
+    Log.write("Jag power output configured to: " + logTime);
+    }
+    public void 
     
 }
