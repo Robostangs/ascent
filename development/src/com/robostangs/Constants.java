3b390e1121fdfd13e8d7643d875ea54f3b42d652
esoterik <nightskyla@gmail.com>
2013-04-03 16:54:26 -0400

    read constants from file ACTUALLY DONE w00t

--- development/src/com/robostangs/Constants.java
+++ development/src/com/robostangs/Constants.java
@@ -13,35 +13,35 @@ public class Constants {
     /*
      * Used in multiple classes
      */
-    public static final double JAG_CONFIG_TIME = 0.5;
-    public static final double BENNETT_CONSTANT = 0.44;
+    public static final double JAG_CONFIG_TIME = ReadConstants.getJagConfigTime();
+    public static final double BENNETT_CONSTANT = ReadConstants.getBennettConstant();
 
     /*
      * Arm
      */
-    public static final int ARM_JAG_POS = 11;
-    public static final int ARM_POT_PORT = 2;
-    public static final int ARM_POT_MIN_VALUE = 47;
-    public static final int ARM_POT_MAX_VALUE = 700;
-    public static final int ARM_POT_SLOW_VALUE = 330;
-    public static final double ARM_MIN_VOLTAGE = 0.07;
+    public static final int ARM_JAG_POS = ReadConstants.getArmJagPos();
+    public static final int ARM_POT_PORT = ReadConstants.getArmPotPos();
+    public static final int ARM_POT_MIN_VALUE = ReadConstants.getArmPotMinValue();
+    public static final int ARM_POT_MAX_VALUE = ReadConstants.getArmPotMaxValue();
+    public static final int ARM_POT_SLOW_VALUE = ReadConstants.getArmPotSlowValue();
+    public static final double ARM_MIN_VOLTAGE = ReadConstants.getArmMinVoltage();
     // Arm PID
-    public static final double ARM_KP_SMALL = 0.0128;
-    public static final double ARM_KI_SMALL = 0.0000009;
-    public static final double ARM_KD_SMALL = 0.0;
-    public static final double ARM_KP_MED = 0.0157;
-    public static final double ARM_KI_MED = 0.00000001;
-    public static final double ARM_KD_MED = 0.0;
-    public static final double ARM_KP_LARGE = 0.0161;
-    public static final double ARM_KI_LARGE = 0.0;
-    public static final double ARM_KD_LARGE = 0.0;
-    public static final double ARM_KP_CAM = 0.0;
-    public static final double ARM_KI_CAM = 0.0;
-    public static final double ARM_KD_CAM = 0.0;
-    public static final double ARM_MIN_POWER = -0.95;
-    public static final double ARM_MAX_POWER = 0.95;
-    public static final double ARM_PID_POT_MIN = 318;
-    public static final double ARM_PID_POT_MAX = 725;
+    public static final double ARM_KP_SMALL = ReadConstants.getArmKPSmall();
+    public static final double ARM_KI_SMALL = ReadConstants.getArmKISmall();
+    public static final double ARM_KD_SMALL = ReadConstants.getArmKDSmall();
+    public static final double ARM_KP_MED = ReadConstants.getArmKPMed();
+    public static final double ARM_KI_MED = ReadConstants.getArmKIMed();
+    public static final double ARM_KD_MED = ReadConstants.getArmKDMed();
+    public static final double ARM_KP_LARGE = ReadConstants.getArmKPLarge();
+    public static final double ARM_KI_LARGE = ReadConstants.getArmKILarge();
+    public static final double ARM_KD_LARGE = ReadConstants.getArmKDLarge();
+    public static final double ARM_KP_CAM = ReadConstants.getArmKPCam();
+    public static final double ARM_KI_CAM = ReadConstants.getArmKICam();
+    public static final double ARM_KD_CAM = ReadConstants.getArmKDCam();
+    public static final double ARM_MIN_POWER = ReadConstants.getArmMinPower();
+    public static final double ARM_MAX_POWER = ReadConstants.getArmMaxPower();
+    public static final double ARM_PID_POT_MIN = ReadConstants.getArmPIDPotMin();
+    public static final double ARM_PID_POT_MAX = ReadConstants.getArmPIDPotMax();
 
     /**
      * 0.015
@@ -60,61 +60,45 @@ public class Constants {
      * 0
      */
     //positions
-    public static final double ARM_SHOOTING_POS = 430;
-    public static final double ARM_START_POS = 600; //TODO: get actual value
+    public static final double ARM_SHOOTING_POS = ReadConstants.getArmShootingPos();
+    public static final double ARM_START_POS = ReadConstants.getArmStartPos();
 
     /*
      * Autonomous
      */
-    public static final double AUTON_DRIVE_POWER = 0.3;
-    public static final double AUTON_TURN_POWER = 0.5;
+    public static final double AUTON_DRIVE_POWER = ReadConstants.getAutonDrivePower();
+    public static final double AUTON_TURN_POWER = ReadConstants.getAutonTurnPower();
     public static final double AUTON_ARM_POS = ARM_SHOOTING_POS;
     
     /*
      * Conveyors
      */
-    public static final int CONV_SHOOT_JAG_POS = 12;
-    public static final int CONV_INGEST_JAG_POS = 13;
-    public static final double CONV_SHOOTER_POWER = 1.0;
-    public static final double CONV_INGEST_POWER = 0.95;
+    public static final int CONV_SHOOT_JAG_POS = ReadConstants.getConvShootJagPos();
+    public static final int CONV_INGEST_JAG_POS = ReadConstants.getConvIngestJagPos();
+    public static final double CONV_SHOOTER_POWER = ReadConstants.getConvShooterPower();
+    public static final double CONV_INGEST_POWER = ReadConstants.getConvIngestPower();
     
     /*
      * Camera
      */
-    public static final int CAM_Y_OFFSET = 0;
-    public static final int CAM_X_OFFSET = 0;
+    public static final int CAM_Y_OFFSET = ReadConstants.getCamYOffset();
+    public static final int CAM_X_OFFSET = ReadConstants.getCamXOffset();
 
     /*
      * DriveTrain
      */
     //Positions
-    public static final int DT_LEFT_ENCODER_FRONT = 2;
-    public static final int DT_LEFT_ENCODER_BACK = 3;
-    public static final int DT_RIGHT_ENCODER_FRONT = 4;
-    public static final int DT_RIGHT_ENCODER_BACK = 5;
-    public static final int DT_GYRO_POS = 0;
-    public static final int DT_JAG_LEFT_FRONT_POS = 4; //left drive 1
-    public static final int DT_JAG_LEFT_MID_POS = 5; //left drive 2
-    public static final int DT_JAG_LEFT_BACK_POS = 6; //left drive 3
-    public static final int DT_JAG_RIGHT_FRONT_POS = 7; //right drive 1
-    public static final int DT_JAG_RIGHT_MID_POS = 8; // right drive 2
-    public static final int DT_JAG_RIGHT_BACK_POS = 9; //right drive 3
-    public static final int DT_JAG_CLIMB_POS = 14;
-    
-    //Drive Straight
-    public static final double DT_STRAIGHT_LEFT_INC = 0.0;
-    public static final double DT_STRAIGHT_LEFT_DEC = 0.0;
-    public static final double DT_STRAIGHT_RIGHT_INC = 0.0;
-    public static final double DT_STRAIGHT_RIGHT_DEC = 0.0;
-
-    //Timer
-    public static final double DT_CONV_VOLT_TO_M_PER_SEC = 0.0;
-    public static final double DT_DELAY_TIME = 1.0;
-    
-    //PID
-    public static final double DT_PID_K_P = 0.0;
-    public static final double DT_PID_K_I = 0.0;
-    public static final double DT_PID_K_D = 0.0;
+    public static final int DT_LEFT_ENCODER_FRONT = ReadConstants.getDTLeftEncoderFront();
+    public static final int DT_LEFT_ENCODER_BACK = ReadConstants.getDTLeftEncoderBack();
+    public static final int DT_RIGHT_ENCODER_FRONT = ReadConstants.getDTRightEncoderFront();
+    public static final int DT_RIGHT_ENCODER_BACK = ReadConstants.getDTRightEncoderBack();
+    public static final int DT_GYRO_POS = ReadConstants.getDTGyroPos();
+    public static final int DT_JAG_LEFT_FRONT_POS = ReadConstants.getDTJagLeftFrontPos();
+    public static final int DT_JAG_LEFT_MID_POS = ReadConstants.getDTJagLeftMidPos();
+    public static final int DT_JAG_LEFT_BACK_POS = ReadConstants.getDTJagLeftBackPos();
+    public static final int DT_JAG_RIGHT_FRONT_POS = ReadConstants.getDTJagRightFrontPos();
+    public static final int DT_JAG_RIGHT_MID_POS = ReadConstants.getDTJagRightMidPos();
+    public static final int DT_JAG_RIGHT_BACK_POS = ReadConstants.getDTJagRightBackPos();
     
     /*
      * Ingestor
@@ -124,39 +108,39 @@ public class Constants {
     /*
      * Lifter
      */
-    public static final int LIFTER_JAG_POS = 10;
-    public static final double LIFTER_UP_POWER = 0.4;
-    public static final double LIFTER_DOWN_POWER = 0.8;
-    public static final double LIFTER_UP_TIME = 2;
-    public static final double LIFTER_DOWN_TIME = 1.6;
-    public static final int LIFTER_TOP_PROX_DIGITAL_PORT = 2;
-    public static final int LIFTER_TOP_PROX_SOLENOID_PORT = 2;
-    public static final int LIFTER_BOTTOM_PROX_DIGITAL_PORT = 1;
-    public static final int LIFTER_BOTTOM_PROX_SOLENOID_PORT = 1;
+    public static final int LIFTER_JAG_POS = ReadConstants.getLifterJagPos();
+    public static final double LIFTER_UP_POWER = ReadConstants.getLifterUpPower();
+    public static final double LIFTER_DOWN_POWER = ReadConstants.getLifterDownPower();
+    public static final double LIFTER_UP_TIME = ReadConstants.getLifterUpTime();
+    public static final double LIFTER_DOWN_TIME = ReadConstants.getLifterDownTime();
+    public static final int LIFTER_TOP_PROX_DIGITAL_PORT = ReadConstants.getLifterTopProxDigitalPort();
+    public static final int LIFTER_TOP_PROX_SOLENOID_PORT = ReadConstants.getLifterTopProxSolenoidPort();
+    public static final int LIFTER_BOTTOM_PROX_DIGITAL_PORT = ReadConstants.getLifterBottomProxDigitalPort();
+    public static final int LIFTER_BOTTOM_PROX_SOLENOID_PORT = ReadConstants.getLifterBottomProxSolenoidPort();
         
     /*
      * Shooter
      */ 
-    public static final int SHOOTER_JAG1_POS = 2;
-    public static final int SHOOTER_JAG2_POS = 1;
-    public static final int SHOOTER_JAG3_POS = 3;
+    public static final int SHOOTER_JAG1_POS = ReadConstants.getShooterJagOnePos();
+    public static final int SHOOTER_JAG2_POS = ReadConstants.getShooterJagTwoPos();
+    public static final int SHOOTER_JAG3_POS = ReadConstants.getShooterJagThreePos();
 
-    public static final double SHOOTER_MAX_POWER = 1.0;
-    public static final double SHOOTER_FEED_POWER = 0.22;
-    public static final double SHOOTER_FULL_BATTERY_VOLTAGE = 14.0;
-    public static final double SHOOTER_VOLTAGE_TOLERANCE = 0.0;
-    public static final double SHOOTER_READY_CURRENT_MAX = 0.0;
-    public static final double SHOOTER_READY_CURRENT_MIN = 0.0; 
+    public static final double SHOOTER_MAX_POWER = ReadConstants.getShooterMaxPower();
+    public static final double SHOOTER_FEED_POWER = ReadConstants.getShooterFeedPower();
+    public static final double SHOOTER_FULL_BATTERY_VOLTAGE = ReadConstants.getShooterFullBatteryVoltage();
+    public static final double SHOOTER_VOLTAGE_TOLERANCE = ReadConstants.getShooterVoltageTolerance();
+    public static final double SHOOTER_READY_CURRENT_MAX = ReadConstants.getShooterReadyCurrentMax();
+    public static final double SHOOTER_READY_CURRENT_MIN = ReadConstants.getShooterReadyCurrentMin();
     //GOOD SHOOTING ANGLE: 247
     
     /*
      * XboxController
      */
     //Driver
-    public static final double XBOX_DRIVER_DRIFT = 0.2;
-    public static final int XBOX_DRIVER_PORT = 1;
+    public static final double XBOX_DRIVER_DRIFT = ReadConstants.getXboxDriverDrift();
+    public static final int XBOX_DRIVER_PORT = ReadConstants.getXboxDriverPort();
     
     //Manipulator
-    public static final int XBOX_MANIP_PORT = 2;
-    public static final double XBOX_MANIP_DRIFT = 0.15;
+    public static final int XBOX_MANIP_PORT = ReadConstants.getXboxManipPort();
+    public static final double XBOX_MANIP_DRIFT = ReadConstants.getXboxManipDrift();
 }
