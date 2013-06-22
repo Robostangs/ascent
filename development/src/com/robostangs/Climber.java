f222784cbf635714c664b07636711a6f68ebc7d4
esoterik <nightskyla@gmail.com>
2013-06-22 08:57:03 -0400

    cleaned climber

--- development/src/com/robostangs/Climber.java
+++ development/src/com/robostangs/Climber.java
@@ -14,18 +14,11 @@ public class Climber {
     private static Climber instance = null;
 	private static Servo leftServo;
     private static Servo rightServo;
-    private static double initRight, initLeft;
     
 	private Climber(){
-        System.out.println("left servo pos? " + Constants.CLIMBER_LEFT_SERVO_POS);
-        System.out.println("right servo pos? " + Constants.CLIMBER_RIGHT_SERVO_POS);
-        //leftServo = new Servo(Constants.CLIMBER_LEFT_SERVO_POS);
-        //rightServo = new Servo(Constants.CLIMBER_RIGHT_SERVO_POS);
-        leftServo = new Servo(1);
-        rightServo = new Servo(2);
-        leftServo.set(1);
-        initLeft = leftServo.get();
-        initRight = rightServo.get();
+        leftServo = new Servo(Constants.CLIMBER_LEFT_SERVO_POS);
+        rightServo = new Servo(Constants.CLIMBER_RIGHT_SERVO_POS);
+        leftServo.set(Constants.CLIMBER_LEFT_IN_POS);
 	}
     
 	public static Climber getInstance() {
@@ -35,35 +28,13 @@ public class Climber {
 		return instance;
 	}
     
-        public static void holdInit() {
-            System.out.println(initLeft + " " + initRight);
-            leftServo.set(initLeft);
-            rightServo.set(initRight);
-        }
+    public static void holdInit() {
+        leftServo.set(Constants.CLIMBER_LEFT_IN_POS);
+        rightServo.set(Constants.CLIMBER_RIGHT_IN_POS);
+    }
         
 	public static void deploy() {
-            leftServo.set(0);
-            rightServo.set(1);
-            /*
-            if (leftServo.get() == 0) {
-                leftServo.set(1);
-            } else {
-                leftServo.set(0);
-            }
-            if (rightServo.get() == 0) {
-                rightServo.set(1);
-            } else {
-                rightServo.set(0);
-            }       */
+        leftServo.set(Constants.CLIMBER_LEFT_OUT_POS);
+        rightServo.set(Constants.CLIMBER_RIGHT_OUT_POS);
 	}
-    
-	public static void retract() {
-        leftServo.set(0);
-        //rightServo.set(Constants.CLIMBER_RIGHT_IN_POS);
-	}
-
-    public static void manual(double input) {
-        leftServo.set(input);
-    }
-    
 }
