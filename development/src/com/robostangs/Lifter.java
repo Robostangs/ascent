e51f34cbc8fcb8d5f24c6509b07599f9526fca1a
esoterik <nightskyla@gmail.com>
2013-06-21 23:43:14 -0400

    cleaned lifter class, removed unecessary methods. began cleaning constants:

--- development/src/com/robostangs/Lifter.java
+++ development/src/com/robostangs/Lifter.java
@@ -13,34 +13,18 @@ import edu.wpi.first.wpilibj.can.CANTimeoutException;
 public class Lifter {
     private static Lifter instance = null;
     private static CANJaguar lift;
-    private static Timer timer;
-    private static boolean atTop;
-    private static boolean atBottom;
-    private static boolean goingToTop;
-    private static boolean goingToBottom;
-    private static int downcount = 0, upCount = 0;
-    private static DigitalInput limit;
     private static ProximitySensor topProx;
     private static ProximitySensor bottomProx;
 
     private Lifter() { 
-        try{
+        try {
             lift = new CANJaguar(Constants.LIFTER_JAG_POS);
         } catch (CANTimeoutException ex) {
             ex.printStackTrace();
         }
-        limit = new DigitalInput(14);
-        timer = new Timer();
-        timer.stop();
-        timer.reset();
-        
-        topProx = new ProximitySensor(9, 1);
-        
-        bottomProx = new ProximitySensor(10, 8);
-        atTop = false;
-        atBottom = true;
-        goingToBottom = false;
-        goingToTop = false;
+
+        topProx = new ProximitySensor(Constants.LIFTER_TOP_PROX_DIGITAL_PORT, Constants.LIFTER_TOP_PROX_SOLENOID_PORT);
+        bottomProx = new ProximitySensor(Constants.LIFTER_BOTTOM_PROX_DIGITAL_PORT, Constants.LIFTER_BOTTOM_PROX_SOLENOID_PORT);
     }
   
     public static Lifter getInstance() {
@@ -51,14 +35,18 @@ public class Lifter {
     }
 
     public static boolean atBottom() {
-        return limit.get();
+        return !bottomProx.get();
+    }
+
+    public static boolean atTop() {
+        return !topProx.get();
     }
    /**
     * Lifter goes up
     */
     public static void raise() {
         try {
-            lift.setX(1);
+            lift.setX(Constants.LIFTER_UP_POWER);
         } catch (CANTimeoutException ex) {
             ex.printStackTrace();
         }
@@ -69,23 +57,29 @@ public class Lifter {
     */
     public static void lower() {
         try {
-            lift.setX(-1);
+            lift.setX(-Constants.LIFTER_DOWN_POWER);
         } catch (CANTimeoutException ex) {
             ex.printStackTrace();
         }
     }
 
-    public static void constantDown() {
+    /**
+     * Lifter down at slow speed
+     */
+    public static void slowDown() {
         try {
-            lift.setX(-0.15);
+            lift.setX(-Constants.LIFTER_SLOW_DOWN_POWER);
         } catch (CANTimeoutException ex) {
             ex.printStackTrace();
         }
     }
 
-    public static void constantUp() {
+    /**
+     * Lifter up at slow speed for maintaining height
+     */
+    public static void slowUp() {
         try {
-            lift.setX(0.09);
+            lift.setX(Constants.LIFTER_SLOW_UP_POWER);
         } catch (CANTimeoutException ex) {
             ex.printStackTrace();
         }
@@ -103,150 +97,67 @@ public class Lifter {
         }
     }
 
-    public static void currentDown() {
-        if(atTop) {
-          atTop = false;
-          lower();
-        }
-
-        System.out.println("jag current: " + getJagCurrent());
-        if (getJagCurrent() >= 1.7 && !atBottom) {
-          lower();
-          goingToBottom = true;
-          downcount++;
-        } else {
-          stop();
-          atBottom = true;
-          goingToBottom = false;
-          downcount = 0;
-        }
-    }
-    
-    public static void currentUp() {
-        System.out.println("current up!");
-        atBottom = !getBottomSensor();
-        if(atBottom) {
-          atBottom = false;
-          raise();
-          System.out.println("if at bottom is true stuff");
-        }
-
-        System.out.println("jag current: " + getJagCurrent());
-        if (getJagCurrent() <= 5.0 && !atTop) {
-          System.out.println("jag current < 5");
-          raise();
-          goingToTop = true;
-        } else if (atTop) {
-            System.out.println("at top is true");
+    /**
+     * Uses the prox sensor to move basket to top position
+     * Runs at low power at top to maintain height
+     */
+    public static void sensorUp() {
+        if (atTop()) {
             constantUp();
         } else {
-            System.out.println("current more than 5!!!");
-          constantUp();
-          atTop = true;
-          goingToTop = false;
+            raise();
         }
     }
 
-  public static void timedDown() {
-      if(atTop) {
-          timer.stop();
-          timer.reset();
-          timer.start();
-          atTop = false;
-      }
-
-      if (timer.get() <= Constants.LIFTER_DOWN_TIME && !atBottom) {
-          lower();
-          goingToBottom = true;
-      } else {
-          stop();
-          atBottom = true;
-          goingToBottom = false;
-      }
-  }
-
-
-  public static void timedUp() {
-      if (atBottom) {
-          timer.stop();
-          timer.reset();
-          timer.start();
-          atBottom = false;
-      }
-
-      if (timer.get() <= Constants.LIFTER_UP_TIME && !atTop) {
-          goingToTop = true;
-          raise();
-      } else {
-          timer.stop();
-          stop();
-          atTop = true;
-          goingToTop = false;
-      }
-  }
-
-  /*
-  public static boolean getTopSensor() {
-      return topProx.get();
-  }*/
-
-  public static boolean getBottomSensor() {
-      return bottomProx.get();
-  }
-  
-  public static void sensorUp() {
-      if (topProx.get()) {
-          raise();
-      } else {
-	  constantUp();
-      }
-  }
-
-  public static void sensorDown() { 
-      if (bottomProx.get()) {
-          lower();
-      } else {
-          stop();
-          atTop = false;
-      }
-  }
-
-  /**
-   * Stops the lifter
-   */
-  public static void stop() {
-    try {
-        lift.setX(0.0);
-    } catch (CANTimeoutException ex) {
-        ex.printStackTrace();
+    /**
+     * Uses the prox sensor to move basket to ingest position
+     */
+    public static void sensorDown() { 
+        if (atBottom()) {
+            stop();
+        } else {
+            lower();
+        }
     }
-  }
 
-  public static void manual(double speed) {
-      timer.stop();
-      atTop = false;
-      atBottom = !getBottomSensor();
-      /*
-      if (goingToBottom || atBottom) {
-          atTop = false;
-      } else if (goingToTop || atTop) {
-          atBottom = false;
-      }
-      */
-      try {
-          lift.setX(speed);
-      } catch (CANTimeoutException ex) {
-          ex.printStackTrace();
-      }
-  }
+    /**
+     * Stops the lifter
+     */
+    public static void stop() {
+        try {
+            lift.setX(0.0);
+        } catch (CANTimeoutException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-  public static double getSetSpeed() {
-      try {
-          return lift.getX();
-      } catch (CANTimeoutException ex) {
-          ex.printStackTrace();
-      } finally {
-          return 0;
-      }
-  }
+    /**
+     * Manual lifter control
+     */
+    public static void manual(double speed) {
+        if (atTop() && speed > 0) {
+            //at top, trying to go up, hold at constant slow speed
+            constantUp();
+            speed = 0;
+        } else if (atBottom() && speed < 0) {
+            //at bottom, trying to go down, don't move basket
+            speed = 0;
+        }
+
+        try {
+            lift.setX(speed);
+        } catch (CANTimeoutException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static double getSetSpeed() {
+        try {
+            return lift.getX();
+        } catch (CANTimeoutException ex) {
+            ex.printStackTrace();
+        } finally {
+            return 0;
+        }
+    }
 }
